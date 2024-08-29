80039768c7c46b8670584f417e74a972a3e9881c
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2024-08-29 08:29:28 -0400

    testing bicep impl

--- .github/workflows/scripts/Deploy-To-Test.ps1
+++ .github/workflows/scripts/Deploy-To-Test.ps1
@@ -87,10 +87,12 @@ $paramNewAzResourceGroupDeployment = @{
     ResourceGroupName = $ResourceGroupName
     #TemplateUri = 'https://raw.githubusercontent.com/Azure/AVDSessionHostReplacer/v0.0.1-beta.0/deploy/arm/DeployAVDSessionHostReplacer.json'
     #TemplateUri = 'https://github.com/ARPA-H/avdsessionhostreplacer-nih/blob/main/deploy/arm/DeployAVDSessionHostReplacer.json'
-    TemplateUri = 'https://raw.githubusercontent.com/Azure/AVDSessionHostReplacer/v0.3.1-beta.1/deploy/arm/DeployAVDSessionHostReplacer.json'
+    
+    # this one works
+    #TemplateUri = 'https://raw.githubusercontent.com/Azure/AVDSessionHostReplacer/v0.3.1-beta.1/deploy/arm/DeployAVDSessionHostReplacer.json'
 
     # If you cloned the repo and want to deploy using the bicep file use this instead of the above line
-    #TemplateFile = '.\deploy\bicep\DeployAVDSessionHostReplacer.bicep'
+    TemplateFile = '.\deploy\bicep\DeployAVDSessionHostReplacer-arpah.bicep'
     TemplateParameterObject = $TemplateParameters
 }
 
